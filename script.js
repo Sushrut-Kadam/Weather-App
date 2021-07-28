7cda69660bb9ab32439db4b1ecb6354598f59aa9
Sushrut Kadam <sushrutkadam.123@gmail.com>
2021-07-28 13:45:14 +0530

    styling added

--- script.js
+++ script.js
@@ -5,49 +5,47 @@ const input = document.querySelector("#input form input");
 form.addEventListener("submit", e => {
     e.preventDefault();
     const inputVal = input.value;
-    
-    console.log("Working");
-    console.log(inputVal);
-    // console.log(input.value);
 
     const apiKey = "2c09af89cc7953eb2917b4239b1fade4";
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric `;
 
     fetch(url
     ).then(response => response.json()
     ).then(data => {
-            const { main, name, sys, weather } = data;
+        const { main, name, sys } = data;
 
-            const li = document.createElement("li");
-            li.classList.add("city");
-            const markup = `
+        const li = document.createElement("li");
+        li.classList.add("city");
+        const markup = `
                 <h2 class="city-name" data-name="${name}, ${sys.country}">
                     <span>${name}</span>
-                    <sup>${sys.country}</sup>
+                    <span>${sys.country}</span>
                 </h2>
                 <div class="city-temp">${main.temp}<sup>°C</sup>
                 </div>
                 `;
 
 
-            const list = document.querySelector(".cities");
+        const list = document.querySelector(".cities");
 
-            console.log(list);
+        console.log(list);
 
-            li.innerHTML = markup;
-            list.appendChild(li);
-        })
+        li.innerHTML = markup;
+        list.appendChild(li);
+    })
         .catch(() => {
-            message.textContent = "Invalid City Name";
+            message.textContent = "Invalid City Name !";
         });
+
+
+
+    const message = document.querySelector(".message");
+
+    message.textContent = "";
+    form.reset();
+    input.focus();
 });
 
-
-const message = document.querySelector(".message");
-
-message.textContent = "";
-form.reset();
-input.focus();
 
 
