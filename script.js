760ddd1c460cbe72ebf62302619db9e9da552301
Sushrut Kadam <sushrutkadam.123@gmail.com>
2021-07-30 16:01:56 +0530

    UI changes and script refactored

--- script.js
+++ script.js
@@ -1,50 +1,54 @@
 const form = document.querySelector("#input form");
 const input = document.querySelector("#input form input");
 
-
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const inputVal = input.value;
-
+function getWeatherData(cityName) {
+    
     const apiKey = "2c09af89cc7953eb2917b4239b1fade4";
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric `;
-
-    fetch(url
-    ).then(response => response.json()
-    ).then(data => {
-        const { main, name, sys } = data;
-
-        const li = document.createElement("li");
-        li.classList.add("city");
-        const markup = `
-                <h2 class="city-name" data-name="${name}, ${sys.country}">
-                    <span>${name}</span>
-                    <span>${sys.country}</span>
-                </h2>
-                <div class="city-temp">${main.temp}<sup>°C</sup>
-                </div>
-                `;
-
-
-        const list = document.querySelector(".cities");
-
-        console.log(list);
-
-        li.innerHTML = markup;
-        list.appendChild(li);
-    })
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            displayWeatherData(data);
+        })
         .catch(() => {
             message.textContent = "Invalid City Name !";
         });
 
+        const message = document.querySelector(".message");
+        message.textContent = "";
+        form.reset();
+        input.focus();
+}
 
+function displayWeatherData(data) {
+    const {main, name, sys, weather} = data;
 
-    const message = document.querySelector(".message");
+    const li = document.createElement("li");
+    li.classList.add("city");
+    const markup = `
+                <h2 class="city-name">
+                    <span>${name}</span>
+                    <span>${sys.country}</span>
+                </h2>
+                <div class="city-temp">${main.temp}<sup>°C</sup></div>
+                <div class="icon">
+                    <img src="./icons/${weather[0].icon}.png" />
+                    <p>${weather[0].description}</p>
+                </div>
+                `;
 
-    message.textContent = "";
-    form.reset();
-    input.focus();
+    li.innerHTML = markup;
+
+    const list = document.querySelector(".cities");
+    list.appendChild(li);
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    getWeatherData(input.value);
 });
 
 
